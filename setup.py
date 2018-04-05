2e7b463433125c281e9bf928ec287d8b03c8790c
Nick McCoy <nick@stitchdata.com>
2018-04-05 15:06:08 -0400

    bumping version to 0.3.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-quickbase',
-      version='0.3.4',
+      version='0.3.5',
       description='Singer.io tap for extracting data from QuickBase',
       author='Stitch',
       url='https://singer.io',
