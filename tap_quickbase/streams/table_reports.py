a1e5bc1653eb733d651ab982569d9a046853f0d3
Tushar Mittal <tushar.mittal@qlik.com>
2026-01-07 17:41:12 +0530

    key properties correction

--- tap_quickbase/streams/table_reports.py
+++ tap_quickbase/streams/table_reports.py
@@ -5,8 +5,14 @@ from tap_quickbase.streams.abstracts import FullTableStream
 class TableReports(FullTableStream):
     """TableReports stream."""
     tap_stream_id = "table_reports"
-    key_properties = ["id"]
+    key_properties = ["tableId", "id"]
     replication_method = "FULL_TABLE"
     replication_keys = []
     path = "v1/reports?tableId={tableId}"
     parent = "app_tables"
+
+    def modify_object(self, record, parent_record=None):
+        """Add tableId from parent record to make composite primary key."""
+        if parent_record:
+            record['tableId'] = self._get_table_id(parent_record)
+        return record
