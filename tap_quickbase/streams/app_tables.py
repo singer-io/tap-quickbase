8c634cf5f89ff1513c43efddfae031c96cfbbf28
Tushar Mittal <tushar.mittal@qlik.com>
2026-01-14 13:08:01 +0530

    pseudo incremental full_table

--- tap_quickbase/streams/app_tables.py
+++ tap_quickbase/streams/app_tables.py
@@ -1,14 +1,14 @@
 """App tables stream definition."""
 
-from tap_quickbase.streams.abstracts import ChildBaseStream
+from tap_quickbase.streams.abstracts import FullTableStream
 
 
-class AppTables(ChildBaseStream):
+class AppTables(FullTableStream):
     """App tables stream."""
     tap_stream_id = "app_tables"
     key_properties = ["id"]
-    replication_method = "INCREMENTAL"
-    replication_keys = ["updated"]
+    replication_method = "FULL_TABLE"
+    replication_keys = []
     path = "v1/tables?appId={appId}"
     parent = "apps"
     children = ["tables", "table_relationships", "table_reports", "fields", "fields_usage"]
--- tap_quickbase/streams/apps.py
+++ tap_quickbase/streams/apps.py
@@ -1,14 +1,14 @@
 """Apps stream definition."""
 
-from tap_quickbase.streams.abstracts import IncrementalStream
+from tap_quickbase.streams.abstracts import FullTableStream
 
 
-class Apps(IncrementalStream):
+class Apps(FullTableStream):
     """Apps stream."""
     tap_stream_id = "apps"
     key_properties = ["id"]
-    replication_method = "INCREMENTAL"
-    replication_keys = ["updated"]
+    replication_method = "FULL_TABLE"
+    replication_keys = []
     path = "v1/apps/{appId}"
     children = ["events", "roles", "app_tables"]
 
