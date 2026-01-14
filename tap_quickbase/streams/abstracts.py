1483f238b2d0981422019f1dd9240e62d0933cb8
Tushar Mittal <tushar.mittal@qlik.com>
2026-01-14 15:08:11 +0530

    pylint fix

--- tap_quickbase/streams/abstracts.py
+++ tap_quickbase/streams/abstracts.py
@@ -452,7 +452,7 @@ class PseudoIncrementalStream(BaseStream):
                 transformed_record = transformer.transform(
                     record, self.schema, self.metadata
                 )
-                
+
                 if self.is_selected():
                     write_record(self.tap_stream_id, transformed_record)
                     counter.increment()
